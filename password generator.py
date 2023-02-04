d55b10bd385174c33140e804b26ccc9c26f9abf2
Sulav Katwal <42937315+sulavkc212@users.noreply.github.com>
2023-02-04 18:04:13 +0545

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def password_generator(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(characters) for i in range(length))
+    return password
+
+password = password_generator(16)
+print("Password:", password)
